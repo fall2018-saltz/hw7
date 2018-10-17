3ec6333d66d89014cc3ca1265e827f9572d52e62
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:42:45 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,5 +1,5 @@
 
-# Cleaning the data set given.
+# Cleaning the data set given and renaming the columns.
 raw_data<-raw_data[,-1:-4]
 raw_data<-raw_data[-c(1,53,10),]
 colnames(raw_data) <- c("stateName","population","popOver18","percentOver18")
@@ -9,7 +9,8 @@ arrests<-USArrests
 arrests <- cbind(rownames(arrests),arrests)
 colnames(arrests) <- c("stateName")
 arrests
-# Merging the 2 data sets.
+
+# Merging the 2 data sets and renaming the columns.
 final_data<-merge(raw_data,arrests)
 colnames(final_data) <- c("stateName","population","popOver18","percentOver18","Murder","Assault","UrbanPop","Rape")
 final_data
