6bffbee45ae35687c64dfffed8db6371c20df691
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:02:30 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -48,7 +48,7 @@ murder_map<- ggplot(final_data,aes(map_id=stateName))+ geom_map(map=US,aes(fill=
 murder_map<-murder_map + expand_limits(x=US$long, y=US$lat)
 murder_map
 
-population_bubble<-ggplot(final_data) + geom_point(colour="blue",aes(size=population),x=population,y="") + xlab("Population")
+population_bubble<-ggplot(final_data,aes(size=population)) + geom_point() + xlab("Population")
 
 population_bubble
 
