b9704ea729ff6218bfd5b8652d632195877497e8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:26:06 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -58,10 +58,10 @@ population_bubble<- population_bubble + geom_point(aes(x=final_data$longitude, y
 population_bubble<-population_bubble + ggtitle("Population Bubble Chart")
 population_bubble
 
-northeast_murder=  murder+ xlim(-83.93, -63.93) + ylim(30.73, 50.73)
+northeast_murder=  murder_map+ xlim(-83.93, -63.93) + ylim(30.73, 50.73)
 northeast_murder
 
-northeast_pop= pop + xlim(-83.93, -63.93) + ylim(30.73, 50.73)
-northeast_pop
+northeast_population= population_bubble + xlim(-83.93, -63.93) + ylim(30.73, 50.73)
+northeast_population
 
 
